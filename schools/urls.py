64047f974533f04d0308c32fc94e14e64c21e2ad
suhailvs <suhailvs@gmail.com>
2022-01-30 15:22:48 +0530

    first working version

---
+++ schools/urls.py
@@ -0,0 +1,20 @@
+from django.urls import include, path, re_path
+from . import views as school_views
+from django.views.generic import TemplateView
+
+app_name = 'schools'
+urlpatterns = [
+	# if blank show districts(Thissur, palakkad...)
+	path('', school_views.states, name='states'),	
+
+		
+
+	re_path(r'^(?P<code>\d{4}\w{7})/',school_views.school_view, name='school_view'),
+
+	# if districts show districts(Thissur, palakkad...) of a state
+	path('<slug:state>/', school_views.districts, name='districts'),
+
+	# if character, show sub districts
+	path('<slug:state>/<district>/',school_views.sub_districts, name='sub_districts'),
+	path('<slug:state>/<district>/<sub_district>/',school_views.schools, name='schools'),
+]

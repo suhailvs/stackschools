c8931289afa61d82d447c94f5da2da0ac2799e0f
suhailvs <suhailvs@gmail.com>
2024-04-12 11:11:11 +0530

    Deleted apps feeds core and activities

--- feeds/templatetags/feedtags.py
+++
@@ -1,7 +0,0 @@
-from django.template.defaultfilters import register
-import hashlib
-@register.filter
-def gravatar_url(email, size=40):
-    # TEMPLATE USE:  {{ email|gravatar_url:150 }}
-    email_hash = hashlib.md5(email.lower().encode('utf-8')).hexdigest()
-    return f"https://www.gravatar.com/avatar/{email_hash}?s={size}&d=identicon"

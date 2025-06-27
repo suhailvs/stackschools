3b5a4335d4ec778f1fd2bc57617982dc226d1c46
suhailvs <suhailvs@gmail.com>
2025-06-27 18:27:46 +0530

    added counter

--- postalcodes/views.py
+++ postalcodes/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import PostalCode
+from bachelorsportal.views import incr_counter
 
 def postalcode_view(request, code):   
+    incr_counter('postalcodes')
     data = PostalCode.objects.filter(postal_code=code)
     return render(request, "postalcodes/postalcode.html", {"data":data})
 

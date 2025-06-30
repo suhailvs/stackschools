efd3026e9cf0a257eb124e4668ee23405af23ce6
suhailvs <suhailvs@gmail.com>
2025-06-30 19:58:10 +0530

    redesign postal codes

--- schools/templatetags/schools_tags.py
+++ schools/templatetags/schools_tags.py
@@ -1,10 +1,15 @@
 
 from django.template.defaultfilters import register
 from schools.models import GeneralSettings
-
+from postalcodes.country_codes import country_code_to_name
 @register.simple_tag
 def google_analytics_status():
     ga = GeneralSettings.objects.filter(key='google_analytics_status').first()
     if ga and ga.value=='true':
         return True
     return False
+
+
+@register.filter(name="getcountryname")
+def getcountryname(value):
+    return country_code_to_name(value)
